80b566790c11fdeddfa95fa613e80a5cfd272c70
Islam <islam.abdlhalim@unity3d.com>
2022-09-21 22:22:33 +0300

    Initial commit

---
+++ Tests/MyProject.Tests/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace MyProject.Tests;
+
+public static class ListExtensions
+{
+    public static string AddItems<T>(this List<string> list)
+    {
+        string totalString = "";
+        foreach (var item in list)
+        {
+            totalString += item;
+        }
+
+        return totalString;
+    }
+    
+}
---
+++ src/MyProject/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace MyProject;
+
+public static class ListExtension
+{
+    public static string ConcatenateList(this List<string> list)
+    {
+        string concatenate = "";
+        foreach (var item in list)
+        {
+            concatenate += item;
+        }
+
+        return concatenate;
+    }
+}
